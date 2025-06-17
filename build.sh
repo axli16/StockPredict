6fad675be73cb9f9ee00ef91d1c24d01fd16d40b
Andrew <88172986+axli16@users.noreply.github.com>
2025-06-16 20:46:55 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gdown --id 1zXOSGYU2iSmNRzKSviIXiySTZUEGc1Z2 -O stock_model.joblib
 pip install -r requirements.txt
+curl -L "https://www.dropbox.com/scl/fi/7nd0bzdizf8f6fhefffbg/stock_model.joblib?rlkey=o45qvsekv3hwbmnfc9rovgjar&st=0qurps1e&dl=1" > stock_model.joblib
